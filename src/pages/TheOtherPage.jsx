61903b214c76a10ce6afc7f466066e1ed96c6bf3
Alison Estephany <alisonestephanyoz@gmail.com>
2023-01-04 01:26:20 -0400

    Add conditional rendering with contional styles defined as a variable

--- src/pages/TheOtherPage.jsx
+++ src/pages/TheOtherPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useState } from "react"
 
 const TheOtherPage = () => {
+
+  const[ yesNO, setYesNo ] = useState(true);
+
+  const handleClick = () => {
+    setYesNo(!yesNO)
+  }
+  
+
+  const conditionalClassName = yesNO ? "bg-green-500" : "bg-red-500"; 
   return (
-    <div className='justify-center flex text-purple-400 font-bold text-3xl'>The Other Page</div>
+    <div className='justify-center mx-auto'>
+      <h2 className="text-purple-400 font-bold text-3xl text-center my-8">The Other Page</h2>
+      <h3 className='text-center p-4 text-purple-400 font-bold'>Conditional rendering with contional styles defined as a variable</h3>
+      <button className={`p-2 font-bold ${conditionalClassName}`} onClick={handleClick}>{yesNO ? "Yes" : "No"} </button>
+
+    </div>
   )
 }
 
